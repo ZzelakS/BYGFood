418b3df7a9989fb8df9bd8a243d4cf7342b2ef65
ZzelakS <96263142+ZzelakS@users.noreply.github.com>
2025-02-23 22:21:32 +0100

    Update ProductInfo.jsx

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -111,7 +111,7 @@ function ProductInfo() {
                           <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
                         </svg>
                       ))}
-                    <span className="text-gray-600 ml-2 text-sm">{product.reviews || 0} Reviews</span>
+                    <span className="text-gray-600 ml-2 text-sm">{product.reviews || 9} Reviews</span>
                   </div>
 
                   <p className="leading-relaxed mt-3 text-sm sm:text-base">{product.description}</p>

ec36e522e43e38c515b4a1193f2d1492ea28f003
ZzelakS <96263142+ZzelakS@users.noreply.github.com>
2025-04-04 20:30:00 +0100

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <div className="w-full min-h-[300px] h-[50vh] md:h-[60vh] lg:h-[80vh] relative flex items-center justify-center">
       <img
-        src="https://ik.imagekit.io/byg/image%20(2)%20(1).png?updatedAt=1743415112098"
+        src="https://ik.imagekit.io/byg/image%20(3)%20(1).png?updatedAt=1743794726222"
         alt="Hero Background"
         className="w-full h-full object-cover"
       />
